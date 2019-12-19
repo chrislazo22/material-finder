3754302f8674d5b54ac2a9124d4fc09f4e086f61
Christian Lazo <lazochristian.cl@gmail.com>
2019-12-18 19:38:21 -0800

    more react stuff

---
+++ app/javascript/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Homepage extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div>
+          <h1>Hello This is the homepage</h1>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Homepage
---
+++ app/javascript/components/_homepage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Homepage = () {
+  return(
+    <div>
+      <h1>Hommmmmeeeeee</h1>
+    </div>
+  )
+}
